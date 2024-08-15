9c8ab1fc6658cb627602147cebebea8d3a88995e
wacho91 <criskol.71@gmail.com>
2024-08-15 11:26:53 -0500

    framer-motion applicating

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,27 @@ import Navbar from "../Navbar/Navbar"
 
 import Blob from "../../assets/blob.svg"
 import heroPng from "../../assets/hero.png"
+import { motion } from "framer-motion"
+
+export const FadeUp = (delay) => {
+    return {
+      initial: {
+        opacity: 0,
+        y: 50,
+      },
+      animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+          type: "spring",
+          stiffness: 100,
+          duration: 0.5,
+          delay: delay,
+          ease: "easeInOut",
+        },
+      },
+    };
+  };
 
 const Hero = () => {
   return (
@@ -12,21 +33,43 @@ const Hero = () => {
         {/*Brand Info*/}
         <div className="flex flex-col justify-center py-14 md:py-0 relative z-20">
             <div className="text-center md:text-left space-y-10 lg:max-w-[400px]">
-                <h1 className="text-3xl lg:text-5xl font-bold !leading-snug">
+                <motion.h1 
+                    variants={FadeUp(0.6)}
+                    initial="initial"
+                    animate="animate"
+                    className="text-3xl lg:text-5xl font-bold !leading-snug"
+                >
                     Let's Learn to build a{" "} <span className="text-secondary">Website</span> for your business
-                </h1>
-                <div className="flex justify-center md:justify-start">
+                </motion.h1>
+                <motion.div
+                    variants={FadeUp(0.8)}
+                    initial="initial"
+                    animate="animate" 
+                    className="flex justify-center md:justify-start"
+                >
                     <button className="primary-btn flex items-center gap-2 group">
                         Get Started
                         <IoIosArrowRoundForward  className="text-xl group-hover:translate-x-2 group-hover:-rotate-45 duration-300"/>
                     </button>
-                </div>
+                </motion.div>
             </div>
         </div>
         {/*Hero Image*/}
         <div className="flex justify-center items-center">
-            <img src={heroPng}  alt=""  className="w-[400px] xl:w-[600px] relative z-10 drop-shadow"/>
-            <img src={Blob} alt=""  className="absolute -bottom-32 w-[800px] md:w-[1500px] z-[1] hidden md:block"/>
+            <motion.img
+                initial={{ x: 50, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.4, ease: "easeInOut" }} 
+                src={heroPng}  alt=""  
+                className="w-[400px] xl:w-[600px] relative z-10 drop-shadow"
+            />
+            <motion.img 
+                initial={{ x: -50, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.2, ease: "easeInOut" }}
+                src={Blob} alt=""  
+                className="absolute -bottom-32 w-[800px] md:w-[1500px] z-[1] hidden md:block"
+            />
         </div>
       </div>
     </section>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 
+import { motion } from "framer-motion";
 import { MdMenu } from "react-icons/md";
+
 
 const NavbarMenu = [
     {
@@ -32,32 +34,36 @@ const NavbarMenu = [
 const Navbar = () => {
   return (
     <nav className="relative z-20">
-    <div className="container py-10 flex justify-between items-center">
-        {/*Logo Section*/}
-        <div>
-            <h1 className="font-bold text-2xl">
-                CrisKol Design
-            </h1>
-        </div>
-        {/*Menu Section*/}
-        <div className="hidden lg:block">
-            <ul className="flex items-center gap-3">
-                {NavbarMenu.map((menu) => (
-                    <li key={menu.id}>
-                        <a href={menu.path} className="inline-block py-2 px-3 cursor-pointer hover:text-secondary relative group">
-                            <div className="w-2 h-2 bg-secondary absolute mt-2 rounded-full left-1/2 bottom-0 group-hover:block hidden"></div>
-                            {menu.title}
-                        </a>
-                    </li>
-                ))}
-                <button className="primary-btn">Sign In</button>
-            </ul>
-        </div>
-        {/*Mobilee Hamburger menu section*/}
-        <div className="lg:hidden">
-            <MdMenu  className="text-4xl"/>
-        </div>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y:-50 }}
+      animate={{ opacity: 1, y:0 }}
+      className="container py-10 flex justify-between items-center"
+    >
+      {/*Logo Section*/}
+      <div>
+          <h1 className="font-bold text-2xl">
+              CrisKol Design
+          </h1>
+      </div>
+      {/*Menu Section*/}
+      <div className="hidden lg:block">
+          <ul className="flex items-center gap-3">
+              {NavbarMenu.map((menu) => (
+                  <li key={menu.id}>
+                      <a href={menu.path} className="inline-block py-2 px-3 cursor-pointer hover:text-secondary relative group">
+                          <div className="w-2 h-2 bg-secondary absolute mt-2 rounded-full left-1/2 bottom-0 group-hover:block hidden"></div>
+                          {menu.title}
+                      </a>
+                  </li>
+              ))}
+              <button className="primary-btn">Sign In</button>
+          </ul>
+      </div>
+      {/*Mobilee Hamburger menu section*/}
+      <div className="lg:hidden">
+          <MdMenu  className="text-4xl"/>
+      </div>
+    </motion.div>
     </nav>
   )
 }
